2ed541ad0188ff10f7e72c24d14d26fd354b9a9d
Harrison Coulter <harrison@Harrisons-MacBook-Pro.local>
2018-10-24 15:19:44 +0100

    fixed all tests

--- spec/models/upper_right_tile_spec.rb
+++ spec/models/upper_right_tile_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Tile::UpperRightTile, type: :model do
 
   before :each do
     @board = Board.new(id: 1, game_id: 1,tile_set: 'default')
-    @tile = Tile::UpperRightTile.new(1 , 1, @board )
+    @tile = Tile::UpperRightTile.new(1, @board, 1 )
   end
 
   it "tests we can create an instance of UpperRightTile Tile" do
@@ -12,13 +12,13 @@ RSpec.describe Tile::UpperRightTile, type: :model do
   end
 
   it "tests position is correct for the first upper right tile" do
-  	@tile = Tile::LowerRightTile.new(21 , 1, @board )
+  	@tile = Tile::LowerRightTile.new(21, @board, 1 )
     expect(@tile.id).to eq(21)
   end
 
   it "tests if upper right tile has a link" do
-  	@tile = Tile::LowerLeftTile.new(11 , 1, @board )
+  	@tile = Tile::LowerLeftTile.new(11, @board, 1 )
   	expect(@tile.link).to eql 1
   end
 
- end 
+ end
