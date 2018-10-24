c56660366a13e2b439395e6011a5a255a1754935
Chris Simpson <chris.simpson@onthebeach.co.uk>
2018-10-24 15:17:43 +0100

    Add Mr Beach and split out tests

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -17,23 +17,43 @@ RSpec.describe Board, type: :model do
     expect(@board.get_tile_set.first.id).to eq(1)
   end
 
-  it "calls the right tile type based on tiles stored in active record" do
+  it "creates a normal tile type based on stored tiles" do
     Tiles.create(id: 1, tile_type: 'none', link_id: 0)
-    Tiles.create(id: 2, tile_type: 'positive', link_id: 0)
-    Tiles.create(id: 3, tile_type: 'negative', link_id: 0)
-    Tiles.create(id: 4, tile_type: 'lrc', link_id: 0)
-    Tiles.create(id: 5, tile_type: 'urc', link_id: 0)
-    Tiles.create(id: 6, tile_type: 'llc', link_id: 0)
-    Tiles.create(id: 7, tile_type: 'ulc', link_id: 0)
-    Tiles.create(id: 8, tile_type: '', link_id: 0)
-
     expect(@board.get_tile_set[0].class).to eq(Tile)
-    expect(@board.get_tile_set[1].class).to eq(Tile::PositiveTile)
-    expect(@board.get_tile_set[2].class).to eq(Tile::NegativeTile)
-    expect(@board.get_tile_set[3].class).to eq(Tile::LowerRightTile)
-    expect(@board.get_tile_set[4].class).to eq(Tile::UpperRightTile)
-    expect(@board.get_tile_set[5].class).to eq(Tile::LowerLeftTile)
-    expect(@board.get_tile_set[6].class).to eq(Tile::UpperLeftTile)
-    expect(@board.get_tile_set[7].class).to eq(Tile)
+  end
+
+  it "creates a positive tile type based on stored tiles" do
+    Tiles.create(id: 1, tile_type: 'positive', link_id: 0)
+    expect(@board.get_tile_set[0].class).to eq(Tile::PositiveTile)
+  end
+
+  it "creates a negative tile type based on stored tiles" do
+    Tiles.create(id: 1, tile_type: 'negative', link_id: 0)
+    expect(@board.get_tile_set[0].class).to eq(Tile::NegativeTile)
+  end
+
+  it "creates a lower right corner tile type based on stored tiles" do
+    Tiles.create(id: 1, tile_type: 'lrc', link_id: 0)
+    expect(@board.get_tile_set[0].class).to eq(Tile::LowerRightTile)
+  end
+
+  it "creates a upper right corner tile type based on stored tiles" do
+    Tiles.create(id: 1, tile_type: 'urc', link_id: 0)
+    expect(@board.get_tile_set[0].class).to eq(Tile::UpperRightTile)
+  end
+
+  it "creates a lower left corner tile type based on stored tiles" do
+    Tiles.create(id: 1, tile_type: 'llc', link_id: 0)
+    expect(@board.get_tile_set[0].class).to eq(Tile::LowerLeftTile)
+  end
+
+  it "creates a upper left corner tile type based on stored tiles" do
+    Tiles.create(id: 1, tile_type: 'ulc', link_id: 0)
+    expect(@board.get_tile_set[0].class).to eq(Tile::UpperLeftTile)
+  end
+
+  it "creates a normal tile type if no type specified" do
+    Tiles.create(id: 1, tile_type: '', link_id: 0)
+    expect(@board.get_tile_set[0].class).to eq(Tile)
   end
 end
